ca8d3cc9b3a00a3b2fdb99d15c0ede9090417dc8
Maximilian Pensel <maximilian.pensel@gmx.de>
2020-04-20 18:32:33 +0200

    Bump patch version

--- src/scrapy_wrapper.py
+++ src/scrapy_wrapper.py
@@ -60,7 +60,7 @@ if DEBUG:
 else:
     log_level = logging.INFO
 
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 
 # Prepare logging, before reading specification only log on console
 MLOG = shared.simple_logger(loger_name="scrapy_wrapper")
